e7d7a010a680d44778157b6d5551bb716fe7838d
Saunaka Rishi <saunakarishi@shauns-imac.local>
2018-11-16 16:35:15 -0500

    Initial Commit

---
+++ NyackMagic8Tests/NyackMagic8Tests.swift
@@ -0,0 +1,34 @@
+//
+//  NyackMagic8Tests.swift
+//  NyackMagic8Tests
+//
+//  Created by Saunaka Rishi on 11/16/18.
+//  Copyright © 2018 Saunaka Rishi. All rights reserved.
+//
+
+import XCTest
+@testable import NyackMagic8
+
+class NyackMagic8Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
